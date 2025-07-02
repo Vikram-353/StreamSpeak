17e4b3ab6b2ab3145839e91f187d1bd76eb8a4ea
Vikram <vikramranjan71122@gmail.com>
2025-07-02 17:34:10 +0530

    folder setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,107 @@
+import { upsertStreamUser } from "../lib/stream.js";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+export async function signup(req, res) {
+  const { email, password, fullname } = req.body;
+
+  try {
+    if (!email || !password || !fullname) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be 6 digit long." });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exist." });
+    }
+    const idx = Math.floor(Math.random() * 100) + 1; //generate a no. between 1 and 100
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    const newUser = await User.create({
+      email,
+      fullname,
+      password,
+      profilePic: randomAvatar,
+    });
+
+    try {
+      await upsertStreamUser({
+        id: newUser._id.toString(),
+        name: newUser.fullname,
+        image: newUser.profilePic || "",
+      });
+      console.log(`Stream user created fro ${newUser.fullname}`);
+    } catch (error) {
+      console.error("Error creating Stream user:", error);
+    }
+
+    const token = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "7d" }
+    );
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, //prevents XSS attacks
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(201).json({ success: true, user: newUser });
+  } catch (error) {
+    console.log("Error in signUp conroller", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+export async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentilas." });
+    }
+    const isPasswordCorrect = await user.matchPassword(password);
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, //prevents XSS attacks
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.log("Error in login conroller", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+export function logout(req, res) {
+  res.clearCookie("jwt");
+  res.status(200).json({ success: true, message: "Logout successfully" });
+}
---
+++ backend/src/lib/stream.js
@@ -0,0 +1,24 @@
+import { StreamChat } from "stream-chat";
+
+import "dotenv/config";
+
+const apiKey = process.env.STREAM_KEY;
+const apiSecret = process.env.STREAM_SECRET;
+
+if (!apiKey || !apiSecret) {
+  console.error("Stream API key or secret is missing.");
+}
+const streamClient = StreamChat.getInstance(apiKey, apiSecret);
+
+export const upsertStreamUser = async (userData) => {
+  try {
+    await streamClient.upsertUsers([userData]); //upsert-create or update
+    return userData;
+  } catch (error) {
+    console.error("Error upserting userdata", error);
+  }
+};
+
+//TODO:
+
+export const generateStreamToken = (userId) => {};
